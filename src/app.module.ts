fdbf39ad9d8266f2b630a2868c586f3afbf9854e
alabenhamouda <ala.benhamouda@credium.de>
2023-12-06 16:39:27 +0100

    add prometheus

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, PrometheusModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
